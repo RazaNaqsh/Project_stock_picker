a8a075d04f44b4a65171e554f661db62e22ba11f
Raza <owaisraza1704@gmail.com>
2022-09-01 19:09:30 +0530

    Stock picker initial loop

---
+++ stock_picker.rb
@@ -0,0 +1,13 @@
+
+
+stocks = %w[1 5 8 3 5 10]
+highest_profit = 0
+def stock_picker(stocks)
+stocks.each do |stock|
+  p stock
+stocks.each {|stock| p stock}
+  p "end of loop"
+end
+
+end
+stock_picker(stocks)
