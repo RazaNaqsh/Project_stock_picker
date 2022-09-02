81ee3500ecce750c21b70445be64129f93db678b
Raza <owaisraza1704@gmail.com>
2022-09-02 09:49:57 +0530

    Stock picker done ig

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,13 +1,24 @@
 
-
-stocks = %w[1 5 8 3 5 10]
-highest_profit = 0
+stocks = [1,2,3,10,4,7,11,14,16,9]
 def stock_picker(stocks)
-stocks.each do |stock|
-  p stock
-stocks.each {|stock| p stock}
-  p "end of loop"
-end
+    
+    highest_profit = 0
+    index_highest= []
+    stocks.each_with_index do |stock1,ind1|
+        p stock1
 
+        stocks.each_with_index do |stock2,ind2|
+            p stock2
+            if stock2-stock1 > highest_profit
+                highest_profit = stock2 - stock1
+                index_highest = [ind1,ind2]
+            end
+        end
+     
+        p "end of loop"
+    end
+    p "Highest profit: buy at #{stocks[index_highest[0]]}  sell at #{stocks[index_highest[1]]}. Profit of #{highest_profit}"
+    p "and the days are : #{index_highest}"
+    
 end
 stock_picker(stocks)
